4516224760ceb4d48d669fa82f733da39d1b48eb
Danniel Navas <dannielnavas@gmail.com>
2024-08-26 07:49:37 -0500

    ✨ feat: Add lazy loading for images in MainProduct component
    The MainProduct component has been updated to include lazy loading for images. This improves the page load performance by loading images only when they are visible on the screen.

--- src/Components/home/MainProducts/MainProduct.tsx
+++ src/Components/home/MainProducts/MainProduct.tsx
@@ -1,3 +1,13 @@
+import Image from "next/image";
+import {
+  AwaitedReactNode,
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactNode,
+} from "react";
+import styles from "./MainProducts.module.sass";
+
 const getProducts = async () => {
   try {
     const response = await fetch(
@@ -10,22 +20,45 @@ const getProducts = async () => {
         }),
       }
     );
-    const data = await response.json();
-    console.log(data);
-    return data;
+    const { products } = await response.json();
+    return products;
   } catch (error) {
     console.error(error);
   }
 };
 
 const MainProduct = async () => {
-  const product = await getProducts();
-  console.log(product);
-  // console.log(process.env.NEXT_PUBLIC_API_URL);
+  const products = await getProducts();
   return (
-    <div className="main-product">
-      <h1>Main Product</h1>
-    </div>
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products?.map(
+          (product: {
+            images: { src: any }[];
+            id: Key | null | undefined;
+            title:
+              | string
+              | number
+              | bigint
+              | boolean
+              | ReactElement<any, string | JSXElementConstructor<any>>
+              | Iterable<ReactNode>
+              | Promise<AwaitedReactNode>
+              | null
+              | undefined;
+          }) => {
+            const imageSrc = product.images[0].src;
+            return (
+              <article key={product.id}>
+                <p>{product.title}</p>
+                <Image src={imageSrc} fill alt={product.title} loading="eager" />
+              </article>
+            );
+          }
+        )}
+      </div>
+    </section>
   );
 };
 
