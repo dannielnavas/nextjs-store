c1e60f0ec8b2b43a0086b09451d580323fc5c091
Danniel Navas <dannielnavas@gmail.com>
2024-08-26 07:34:55 -0500

    ✨ feat: Update MainProduct component to fetch products from Shopify API
    The MainProduct component has been updated to fetch products from the Shopify API using the `NEXT_PUBLIC_API_URL` environment variable. This change improves the functionality of the component by dynamically retrieving the data from the API instead of hardcoding it.

--- src/Components/home/MainProducts/MainProduct.tsx
+++ src/Components/home/MainProducts/MainProduct.tsx
@@ -1,7 +1,27 @@
-"use client";
+const getProducts = async () => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/admin/api/2023-10/products.json`,
+      {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": process.env.API_KEY_SHOPIFY || "",
+        }),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const MainProduct = () => {
-  console.log(process.env.NEXT_PUBLIC_API_URL);
+const MainProduct = async () => {
+  const product = await getProducts();
+  console.log(product);
+  // console.log(process.env.NEXT_PUBLIC_API_URL);
   return (
     <div className="main-product">
       <h1>Main Product</h1>
