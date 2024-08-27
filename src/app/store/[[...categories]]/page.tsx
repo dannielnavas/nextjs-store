051c5f6180c2fc7b8cbeb14c8030943608837eda
Danniel Navas <dannielnavas@gmail.com>
2024-08-27 07:51:31 -0500

    ✨ feat: Update Category component to remove console.log statements and add error handling
    The Category component has been updated to remove unnecessary console.log statements and add error handling. This improves the code cleanliness and ensures that any errors are properly handled.

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -6,8 +6,7 @@ interface CategoryProps {
 }
 
 export default function Category(props: CategoryProps) {
-  console.log(props);
   const { categories } = props.params;
-  console.log(categories);
+  // throw new Error("Error");
   return <h1>Categoria dinámica: {categories}</h1>;
 }
