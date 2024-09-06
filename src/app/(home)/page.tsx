f4a949990edc6824a11b87bbdbff6a61f6beb0a8
Danniel Navas <dannielnavas@gmail.com>
2024-09-06 07:50:38 -0500

    ✨ feat: Agregar metadatos a las páginas de inicio y productos
    Este commit agrega metadatos a las páginas de inicio y productos para mejorar el SEO y la experiencia del usuario. Se agrega un objeto `metadata` en el archivo `page.tsx` de la carpeta `src/app/(home)` que incluye el título, la descripción y las palabras clave relevantes. Esto permite que los motores de búsqueda indexen y muestren correctamente la información de la página. Además, se agrega una función `generateMetadata` en el archivo `page.tsx` de la carpeta `src/app/product/[handle]` que genera metadatos específicos para cada producto. Estos cambios mejoran la visibilidad y la usabilidad de la aplicación.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,14 @@
 import { MainProduct } from "app/Components/home/MainProducts";
+import { Metadata } from "next";
 
 // "use cliente"; // Para que se ejecute en el cliente este y los anidados sobre este
+
+export const metadata: Metadata = {
+  title: "Lolo Store",
+  description: "Welcome to Lolo store",
+  keywords: ["Lolo, store", "products"],
+};
+
 export default function Home() {
   return (
     <main>
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -13,6 +13,29 @@ interface ProductPageProps {
   };
 }
 
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  const id = searchParams.id;
+
+  const products = await getProducts(id);
+  const product = products[0];
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      images: [
+        {
+          url: product.image,
+          alt: product.title,
+        },
+      ],
+    },
+  };
+}
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   // const params = useParams();
   // const serachParams = useSearchParams();
