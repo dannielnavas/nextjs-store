8387d4e6d77a0a839f14b3fc2564bba67797f735
Danniel Navas <dannielnavas@gmail.com>
2024-09-13 07:49:53 -0500

    ✨ Agregar middleware para redireccionar a la página de la tienda si hay un token de acceso

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/login/:path*", "/signup/:path*"],
+};
+
+export function middleware(request: NextRequest) {
+  const cookiesStore = cookies();
+  const accessToken = cookiesStore.get("accessToken")?.value as string;
+  if (accessToken) return NextResponse.redirect(new URL("/store", request.url));
+  // return NextResponse.redirect(new URL("/store", request.url));
+}
